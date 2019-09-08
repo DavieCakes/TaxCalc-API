9c95ec332c008a513abc9b6b4840bb570860600b
David Gordon <d.phi.gordon@gmail.com>
2019-09-08 11:10:34 -0500

    fixed bug where default parameters were hardcoded into the request to the taxee.io api

--- src/main/java/com/dgordon/taxcalc/controllers/RestClient.java
+++ src/main/java/com/dgordon/taxcalc/controllers/RestClient.java
@@ -31,7 +31,7 @@ public class RestClient {
   public String post(String uri, String body) {
     
     LOG.info( (new StringBuilder()).append("recieved request data, uri: "). append(uri).append(" \nbody: ").append(body).toString() );
-    HttpEntity<String> requestEntity = new HttpEntity<String>("state=NC&filing_status=married&pay_periods=26&pay_rate=116500&exemptions=2", headers);
+    HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
     try {
       ResponseEntity<String> responseEntity = rest.exchange(server + "2019", HttpMethod.POST, requestEntity, String.class);
       this.setStatus(responseEntity.getStatusCode());
